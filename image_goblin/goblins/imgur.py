e46b5545eae74e6473b5edf5a185a2d06db6cd3c
xioren <xioren00+imagegoblin@protonmail.com>
2020-05-23 00:04:46 -0700

    fix imgur bugs

--- image_goblin/goblins/imgur.py
+++ image_goblin/goblins/imgur.py
@@ -44,25 +44,32 @@ class ImgurGoblin(MetaGoblin):
                 matches = self.parser.extract_by_regex(self.get(self.clean(target)).content,
                                                        r'(?<=image\s{15}:\s){[^\n]+}(?=,\n)')
                 for match in matches:
-                    image = self.parser.load_json(match)
-                    urls.append(f'{self.BASE_URL}{image["hash"]}{image["ext"]}')
+                    items = self.parser.load_json(match)
+                    urls.append(f'{self.BASE_URL}{items["hash"]}{items["ext"]}')
             else:
                 matches = self.parser.extract_by_regex(self.get(self.clean(target)).content,
                                                        r'(?<=image\s{15}:\s){[^\n]+}(?=,\n)')
                 for match in matches:
                     items = self.parser.load_json(match)
-                    for image in items['album_images']['images']:
-                        urls.append(f'{self.BASE_URL}{image["hash"]}{image["ext"]}')
+                    if items['is_album'] == True:
+                        for item in items['album_images']['images']:
+                            urls.append(f'{self.BASE_URL}{item["hash"]}{item["ext"]}')
+                    else:
+                        urls.append(f'{self.BASE_URL}{items["hash"]}{items["ext"]}')
 
                 if not urls: # sign in probably required -> try bypass
-                    self.logger.log(2, self.NAME, 'attempting bypass')
-                    matches = self.parser.extract_by_regex(self.get(f'{self.clean(target)}/embed?pub=true').content,
-                                                           r'(?<=images\s{12}=\s){[^\n]+}(?=,\n)')
-                    for match in matches:
-                        items = self.parser.load_json(match)
+                    self.logger.log(1, self.NAME, 'attempting bypass')
+                    if '/a/' in target:
+                        matches = self.parser.extract_by_regex(self.get(f'{self.clean(target)}/embed?pub=true').content,
+                                                               r'(?<=images\s{12}=\s){[^\n]+}(?=,\n)')
+                        for match in matches:
+                            items = self.parser.load_json(match)
 
-                        for image in items['images']:
-                            urls.append(f'{self.BASE_URL}{image["hash"]}{image["ext"]}')
+                            for item in items['images']:
+                                urls.append(f'{self.BASE_URL}{item["hash"]}{item["ext"]}')
+                    else:
+                        response = self.get(target)
+                        urls.append(re.search(r'(?<=og:image" content=")[^"\?]+', response.content).group())
 
         for url in urls:
             self.collect(self.prep(url))
